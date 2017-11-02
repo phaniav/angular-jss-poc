b78bc64f57cde6b8c60caad3bd96a590c4ec34b1
Steve <svandenbush@geekhive.com>
2017-11-02 09:21:13 -0400

    pipeline and readme

---
+++ sitecore/VisitorInfoPipeline.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using Sitecore.JavaScriptServices.Configuration;
+using Sitecore.JavaScriptServices.ViewEngine.LayoutService.Pipelines.GetLayoutServiceContext;
+
+namespace JSS.Pipelines
+{
+    public class VisitorInfoPipeline : JssGetLayoutServiceContextProcessor
+    {
+        public VisitorInfoPipeline(IConfigurationResolver configurationResolver) : base(configurationResolver)
+        {
+        }
+
+        protected override void DoProcess(Sitecore.LayoutService.ItemRendering.Pipelines.GetLayoutServiceContext.GetLayoutServiceContextArgs args, AppConfiguration application)
+        {
+            args.ContextData.Add("sessionInfo", new
+            {
+                isAnonymous = !Sitecore.Context.User.IsAuthenticated,
+                deviceInfo = HttpContext.Current.Request.Browser
+            });
+            
+
+        }
+    }
+}
